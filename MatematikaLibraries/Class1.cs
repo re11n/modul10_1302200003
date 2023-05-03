905d6e5ede4377f21f83f1c6ef177a39441bee62
ryan <ryansiahaan@student.telkomuniveristy.ac.id>
2023-05-03 16:54:48 +0700

    Add project files.

---
+++ MatematikaLibraries/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace MatematikaLibraries
+{
+    public static class UtilityLibraries
+    {
+        public static int FPB(int input1, int input2)
+        {
+            int result = input1 % input2;
+            while (result != 0)
+            {
+                input1 = input2;
+                input2 = result;
+                result = input1 % input2;
+            }
+            return input2;
+        }
+
+        public static int KPK(int input1, int input2)
+        {
+            return (input1 * input2) / FPB(input1,input2);
+        }
+
+        public static string Turunan(int[] persamaan)
+        {
+            int n = persamaan.Length - 1;
+            int[] turun = new int[n];
+            for(int i=1; i < n; i++)
+            {
+                turun[i] = persamaan[i] * (n - i);
+            }
+
+            string result = turun[0] + "x^" + (n - 1);
+
+            for(int i = 1; i < n; i++)
+            {
+                if (turun[i] > 0)
+                {
+                    result = result + "+" + turun[i] + "x" + (n-i-1);
+                } else if (turun[i] <0)
+                {
+                    result = result + "-" + (-turun[i]) + "x" + (n-i-1);
+                }
+            }
+            return result;
+        }
+
+        public static string Integral(int[] persamaan)
+        {
+            int n = persamaan.Length;
+            int[] integral = new int[n + 1];
+            for (int i = 1; i < n; i++)
+            {
+                integral[i] = persamaan[i - 1] / i;
+            }
+            string result = "C";
+            for(int i=n;i>=0;i--)
+            {
+                if (integral[i] > 0)
+                {
+                    result = "+" + integral[i] + "x" + (i + 1) + result;
+                } else if (integral[i] < 0)
+                {
+                    result = integral[i] + "x^" + (i+1) + result;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ ShowCase/Program.cs
@@ -0,0 +1,31 @@
+﻿using MatematikaLibraries;
+using System.Diagnostics.Metrics;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*Console.WriteLine("Masukkan angka pertama: ");
+        int input1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Masukkan angka kedua: ");
+        int input2 = int.Parse(Console.ReadLine());
+
+        int fpb1 = UtilityLibraries.FPB(input1, input2);*/
+
+        Console.WriteLine("Hasil FPB: " + UtilityLibraries.FPB(60, 45));
+
+        /*Console.WriteLine("Masukkan angka pertama: ");
+        input1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Masukkan angka kedua: ");
+        input2 = int.Parse(Console.ReadLine());
+
+        int kpk1 = UtilityLibraries.KPK(input1, input2);*/
+
+        Console.WriteLine("Hasil KPK: " + UtilityLibraries.KPK(12, 8));
+        int[] turun = { 1, 4, -12, 9 };
+        Console.WriteLine("Hasil Turunan: " + UtilityLibraries.Turunan(turun));
+        int[] integ = {4,6, -12, 9};
+        Console.WriteLine("Hasil Integral: " + UtilityLibraries.Integral(integ));
+    }
+}
